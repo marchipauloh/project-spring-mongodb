d93c5464112f592f619e4e2546b34eeb7f799e44
Paulo Marchi <marchipaulo@gmail.com>
2022-07-12 17:26:16 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/marchipauloh/psmdb/resources/UserResource.java
+++ src/main/java/com/marchipauloh/psmdb/resources/UserResource.java
@@ -48,7 +48,12 @@ public class UserResource {
 				.buildAndExpand(obj.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
-		
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
