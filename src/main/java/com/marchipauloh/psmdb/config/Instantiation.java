bb7e5eec79c7dce2eba416fb62e10b47a09dffa3
Paulo Marchi <marchipaulo@gmail.com>
2022-07-12 20:12:05 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/marchipauloh/psmdb/config/Instantiation.java
+++ src/main/java/com/marchipauloh/psmdb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.marchipauloh.psmdb.domain.Post;
 import com.marchipauloh.psmdb.domain.User;
+import com.marchipauloh.psmdb.dto.AuthorDTO;
 import com.marchipauloh.psmdb.repository.PostRepository;
 import com.marchipauloh.psmdb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
