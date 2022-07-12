b8d2e358af2f5d9f18e38a3c0070f86cc071889a
Paulo Marchi <marchipaulo@gmail.com>
2022-07-12 20:36:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marchipauloh/psmdb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.marchipauloh.psmdb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marchipauloh.psmdb.domain.Post;
+import com.marchipauloh.psmdb.repository.PostRepository;
+import com.marchipauloh.psmdb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
