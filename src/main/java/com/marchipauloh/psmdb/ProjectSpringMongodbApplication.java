43aba539fc42b392196b7c076c281467659de79c
Paulo Marchi <marchipaulo@gmail.com>
2022-07-11 22:04:19 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/marchipauloh/psmdb/ProjectSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.marchipauloh.psmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marchipauloh/psmdb/ProjectSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchipauloh.psmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
