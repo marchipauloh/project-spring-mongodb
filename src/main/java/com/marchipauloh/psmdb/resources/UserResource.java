f8382eff41c0a1084898163036852a0ff5994e73
Paulo Marchi <marchipaulo@gmail.com>
2022-07-12 20:22:53 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/marchipauloh/psmdb/resources/UserResource.java
+++ src/main/java/com/marchipauloh/psmdb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marchipauloh.psmdb.domain.Post;
 import com.marchipauloh.psmdb.domain.User;
 import com.marchipauloh.psmdb.dto.UserDTO;
 import com.marchipauloh.psmdb.services.UserService;
@@ -63,6 +64,12 @@ public class UserResource {
 		obj = userService.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
