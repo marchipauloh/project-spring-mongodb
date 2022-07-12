99b69cdb73510e1b5f92fb3d485ca10d5855bfab
Paulo Marchi <marchipaulo@gmail.com>
2022-07-12 20:48:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/marchipauloh/psmdb/config/Instantiation.java
+++ src/main/java/com/marchipauloh/psmdb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.marchipauloh.psmdb.domain.Post;
 import com.marchipauloh.psmdb.domain.User;
 import com.marchipauloh.psmdb.dto.AuthorDTO;
+import com.marchipauloh.psmdb.dto.CommentDTO;
 import com.marchipauloh.psmdb.repository.PostRepository;
 import com.marchipauloh.psmdb.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("2018/03/22"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("2018/03/23"), new AuthorDTO(alex));
+		
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
